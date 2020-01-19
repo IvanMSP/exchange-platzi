c532ed8fd405dd8205a4989869b6edb6e28e4df4
Ivan López <ivanlopz11@gmail.com>
2020-01-19 11:56:48 -0600

    Project kick Off

---
+++ src/views/Error.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">¡Ups Compadre no existe el link!</h1>
+    <h1 class="text-gray-700 text-6xl text-center">
+      :(
+    </h1>
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover-underline">
+      volver a la página inicial
+    </router-link>
+  </div>
+</template>
